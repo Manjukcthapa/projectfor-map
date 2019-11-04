551d52e59363fcd172af2eb9c72e96e1f2583f78
Manju KC <kcmanjuthapa@gmail.com>
2019-11-04 15:28:52 -0500

    created SignUp Component

---
+++ map/src/login/LogIn.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const   SignUp = (props) => {
+
+    return(
+        <div>
+            <form>
+                <input/>
+                <input/>
+                <input/>
+                <input/>
+                <button>Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp
+
