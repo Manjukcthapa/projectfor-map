ecb08a94a24520aa1cddb12bdbf4c86b961183a6
Manju KC <kcmanjuthapa@gmail.com>
2019-11-04 17:12:34 -0500

    added inputs on login component

--- map/src/Auth/LogIn.js
+++ map/src/Auth/LogIn.js
@@ -1,11 +1,30 @@
 import React from "react";
 
 const LogIn  = props => {
-
+const[input, setInput] = useState({
+    email:'',
+    password:''
+})
 
     return(
         <div>
-            
+            <input
+              id="email"
+              type="text"
+              label="Email"
+              name="email"
+              placeholder="email"
+              value={input.email}
+              onchange={handleChange}/>
+            <input
+              id="password"
+              type="password"
+              label="password"
+              name="password"
+              placeholder="password"
+              value={input.password}
+              onchange={handleChange}/>
+            <button>Sign In</button>
         </div>
     )
 }
