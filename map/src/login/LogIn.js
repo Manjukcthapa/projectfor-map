5405e12d3a35675edd7735afafd7422b0a337aad
Manju KC <kcmanjuthapa@gmail.com>
2019-11-04 15:39:31 -0500

    added input field to the component

--- map/src/login/LogIn.js
+++ map/src/login/LogIn.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React from "react";
 
-const   SignUp = (props) => {
+const SignUp = props => {
+  return (
+    <div>
+      <form>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          label="Password"
+          placeholder="name"
+        />
+        <input
+          id="email"
+          type="text"
+          label="Email"
+          name="email"
+          placeholder="email"
+        />
+        <input
+          id="password"
+          type="password"
+          label="Password"
+          name="password"
+          placeholder="password"
+        />
+        <input
+          id="password2"
+          name="password2"
+          type="password"
+          label="Conform password"
+          placeholder="password"
+        />
+        <button>Register</button>
+      </form>
+    </div>
+  );
+};
 
-    return(
-        <div>
-            <form>
-                <input/>
-                <input/>
-                <input/>
-                <input/>
-                <button>Register</button>
-            </form>
-        </div>
-    )
-}
-
-export default SignUp
-
+export default SignUp;
