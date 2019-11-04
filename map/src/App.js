5d95317bfc1f44ddd29be033dc9c8d836ede521d
Manju KC <kcmanjuthapa@gmail.com>
2019-11-04 16:58:36 -0500

    created Login component

--- map/src/App.js
+++ map/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import SignUp from './Auth/SignUp'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     
+     <SignUp/>
       </div>
     );
   }
--- map/src/Auth/SignUp.js
+++ map/src/Auth/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 
 const SignUp = props => {
   const [input, setInput] = useState({
